8044781923952d982bb9d3016062056e884980d7
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-01-28 14:54:20 +0200

    Part 3 pbbackend step5

--- pbbackend/index.js
+++ pbbackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -54,6 +56,24 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+
+    const max = 100000
+    const min = 1
+    const generatedId = Math.floor(Math.random() * (max - min) + min);
+
+    const person = {
+      id: generatedId,
+      name: body.name,
+      number: body.number,
+    }
+  
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
