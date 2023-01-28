5c30c6e6ecfc311ce5adcd8b111db8680d1f3b54
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-01-28 15:50:48 +0200

    Part3 pbbackend step7

--- pbbackend/index.js
+++ pbbackend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -25,11 +27,6 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
-
-/*
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})*/
   
 app.get('/api/persons', (request, response) => {
     response.json(persons)
