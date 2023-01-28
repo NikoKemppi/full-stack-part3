5ddbf0b0ff4b2403190e1f1fc3efb031a6fd3ae6
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-01-28 14:31:15 +0200

    Part3 pbbackend step3

--- pbbackend/index.js
+++ pbbackend/index.js
@@ -34,9 +34,19 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/info', (request, response) => {
-    const x = persons.length
     const date = new Date
     response.send(`<div><p>Phonebook has info for ${persons.length} people</p><p>${date}</p></div>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
